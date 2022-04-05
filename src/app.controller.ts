5214cffb08b9b26a1c3e8fe9ebc07caeab769045
Pongkot Sae-li <pongkot.saeli@outlook.com>
2022-04-05 14:04:46 +0700

    feat: resend image

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Render, Res } from '@nestjs/common';
+import { Response } from 'express'
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('home')
+  getHello(): void { }
+
+  @Get('image/:filename')
+  getImage(
+    @Param('filename')
+    filename: string,
+    @Res()
+    response: Response,
+  ): void {
+    const buffer = this.appService.getImage(filename)
+    response.type('png')
+    response.end(buffer, 'binary')
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import * as fs from 'fs'
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getImage(filename: string): Buffer {
+    const path = `htdocs/private/${filename}`
+
+    if (!fs.existsSync(path)) {
+      throw new NotFoundException()
+    }
+
+    return fs.readFileSync(path)
   }
 }
